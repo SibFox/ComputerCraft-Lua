42ad7e1ece7a7415df61e0a83147d8d18ed2e208
SibFox <mr.dastin02@yandex.ru>
2026-03-07 01:09:34 +0300

    9.2

--- installer.lua
+++ installer.lua
@@ -40,9 +40,9 @@ function install(program)
         exit("Program is already installed. Either use the 'delete' or 'update' command", true)
     end
 
-    if #startup > 0 then
+    if string.len(startup) > 0 then
         if fs.exists("startup") then
-            fs.delete("startup")            
+            fs.delete("startup")
         end
         local sfile = fs.open("startup", "w")
         sfile.write(startup)
